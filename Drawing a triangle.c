9ba3c4425ecc94c1a114c50930c7389510aca657
Amine Slimani <88277757+AmineSlimani@users.noreply.github.com>
2021-08-01 16:53:01 +0100

    Add files via upload
    How to draw a triangle by using  loops 
    
    Example : 
    * 
    * * 
    * * *
    * * * *

---
+++ Drawing a triangle.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+main() {
+
+
+int i,j,n;
+
+printf("Donnez nombre des lignes ")
+scanf("%d",&n)
+
+for(i = 0;i<n;i++){
+    for(j=0 ; j<i;j++){
+            printf("*");
+          }
+              printf("\n");
+
+    }
+
+}
