5be6affb44b4561abcb6458a2fc99770e1578981
Amine Slimani <88277757+AmineSlimani@users.noreply.github.com>
2021-08-04 15:55:13 +0100

    Add files via upload

---
+++ Exercice simple sur les tableaux.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+main(){
+float T[20];
+int i;
+
+T[0] = 1000;
+for(i=1;i<=20;i++){
+    T[i] = T[i-1]+  T[i-1]*0.02;
+}
+for(i=0;i<20;i++){
+    printf("%d)  %.2lf \n",i,T[i]);
+}
+
+return 0 ;}
